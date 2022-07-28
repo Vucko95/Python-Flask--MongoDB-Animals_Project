3ad8ce56c65b78ed7396a1921972bc777438b6b7
Ronald Suplina <tyger995@gmail.com>
2022-07-28 21:24:34 +0100

    Go Update v2

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,4 +2,21 @@
 # sudo  PATH=$PATH:/home/vagrant/go/bin
 
 sudo echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bash_profile
+export PATH
+export PATH=$PATH:/usr/local/go/bin
+export GOROOT=/home/vagrant/go
+export GOPATH=/usr/local/go/bin
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+
 source ~/.bash_profile
+
+
+sudo yum install git -y
+
+sudo chown -R vagrant:vagrant /usr/local/go/
+
+#CORRECT COMMAND
+go get go.mongodb.org/mongo-driver/mongo
+
+go get -u github.com/gorilla/mux
