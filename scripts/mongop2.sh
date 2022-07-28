424fc0fd1b3b41c933f12d8db35af9a36fcf0dba
Ronald Suplina <tyger995@gmail.com>
2022-07-28 16:11:19 +0100

    2nd Part MongoDB Script

---
+++ scripts/mongop2.sh
@@ -0,0 +1,34 @@
+# CREATING ADMIN USER 
+mongo admin --eval 'db.dropUser("superadmin")'
+mongo admin --eval 'db.createUser(
+      {
+          user: "superadmin",
+          pwd: "password",
+          roles: [ "root" ]
+      }
+  )'
+
+
+# LOGIN TO MONGO
+#    mongo admin -u superadmin -p password
+
+
+
+# CREATING APP USER 
+mongo admin --eval 'db.createUser(
+      {
+          user: "todo",
+          pwd: "todo",
+          roles: [ { role: "readWrite", db: "test" } ]
+      }
+  )'
+
+
+  ## EDIT /etc/mongd.conf 
+  ##  ADD
+#   security:
+    # authorization: "enabled"
+
+# REPLACE IP SO ONLY APP VM CAN ACCESS MONGODB
+
+sed -i 's/127.0.0.1/192.168.6.1/g' /etc/mongod.conf
